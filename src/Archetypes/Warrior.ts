e6e7fb3aedf872915d32a1b69c8c61d78f97a128
Rafael Aguiar <terule@gmail.com>
2023-01-18 17:53:31 -0300

    Requisito 05

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  static count = 0;
+
+  constructor(name:string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.count += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.count;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,4 +1,13 @@
 import Archetype from './Archetype';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetype;
-export {};
+export {
+  Mage,
+  Warrior,
+  Necromancer,
+  Ranger,
+};
